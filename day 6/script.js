65a6ba99169600860bbde344939a61a2898d2b28
Aditya Kumar <adityadreamer9060@gmail.com>
2024-10-05 18:19:54 +0530

    funcation done

---
+++ day 6/script.js
@@ -0,0 +1,137 @@
+// function sayName(){
+//     var name = 'Aditya'
+//     console.log(name);
+    
+// }
+
+// sayName()
+
+
+// function sum(){
+//     var a = 10;
+//     var b= 20;
+//     var c = (a+b)
+//     console.log(c);
+    
+// }
+// sum()
+
+// function sum (a,b){
+//       console.log(a+b);
+      
+// }
+// sum(10,20)
+// sum(100,200)
+
+// function sayName(Name){
+//     console.log(Name);
+    
+// }
+// sayName('Aditya')
+// sayName('Mohit')
+
+// function multi(a,b){
+//     console.log(a*b);
+    
+// }
+// multi(2,3)
+// multi(20,3)
+// multi(200,3)
+
+// function sub(a,b){
+//     console.log(a-b);
+    
+// }
+// sub(20,10);
+// sub(200,100);
+// sub(200,100);
+
+// function multi(a,b){
+//     var mul = a*b
+//     return mul
+// }
+// var c = multi(2,3)
+// console.log(c);
+
+// function adding(a,b){
+//     var add=(a+b)
+//     return add
+// }
+// var c = adding(5,5)
+// console.log(c);
+
+
+
+// function subtract(a,b){
+//     console.log(a-b);
+    
+// }
+// subtract(200,10)
+
+// function subtract(a,b){
+//     var sub = a-b
+//     return sub
+// }
+// var c = subtract(50,20)
+// console.log(c);
+
+// function multiply(a,b){
+//     var multi = a*b
+//     return multi
+// }
+// var final = multiply(40,20)
+// console.log(final);
+
+
+// function divied(a,b){
+//     var div = a/b
+//     return div
+// }
+// var final = divied (20,2)
+// console.log(final);
+
+
+// function adding(a,b){
+//     var add = a+b
+//     return add
+// }
+// var final = adding (20,20)
+// console.log(final);
+
+
+
+// function multi(a,b){
+//     var mul = a-b;
+//     return mul;
+// }
+// var final = multi(20,5)
+// console.log(final);
+
+// function sum(a,b){
+//     var sum1 = a+b;
+//     return sum1
+// }
+// var sumfinal = sum(10,20)
+// console.log(sumfinal);
+
+// function square(x){
+//     var sq = x * x;
+//     return sq;
+// }
+// var sqfinal = square(sumfinal)
+// console.log(sqfinal);
+
+
+function adding(a,b){
+    var add = a+b;
+    return add
+}
+var c = adding(2,3)
+console.log(c);
+
+function square(x){
+    var sq = x * x ;
+    return sq ;
+}
+var d = square(c)
+console.log(d);
---
+++ revision/script.js
@@ -0,0 +1,95 @@
+// loop
+
+// var a =10;
+// while(a<=20){
+//     console.log(a);
+//     a++
+    
+// }
+
+// for ( var i=10; i<20; i++){
+//     if ( i===15){
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+// Array
+
+// var arr = ['Aditya','Mohit','Aditya','Mohit','Aditya','Mohit'] ;
+// for (var i=0; i<=arr.length-1; i++){
+//     console.log(arr[i]);
+    
+// }
+// var arr = ['Aditya','Mohit','Aditya','Mohit','Aditya','Mohit'] ;
+// for( var i=arr.length-1; i>=0; i--){
+//     console.log(arr[i]);
+    
+// }
+
+// add 
+// var arr = [ 1,2,3,4,5] ;
+// var sum = 0 ;
+// for ( var i=0; i<=arr.length-1; i++){
+//     sum += arr[i]
+// }
+// console.log(sum);
+
+//  var arr = [ 1,2,3,4,5,6,7,8,9] ;
+// var evencount= 0;
+// var oddcount= 0;
+// for (var i=0; i<=arr.length-1; i++){
+//     if( arr[i]%2==0){
+//        evencount++
+//     }
+//     else{
+//         oddcount++
+//     }
+
+// }
+// console.log(evencount,oddcount);
+
+ 
+// String
+
+// var str1 =`Aditya`
+// var str2 = `Kumar`
+// var str3 = (`${str1} ${str2}`)
+// console.log(str[2]);/
+// console.log(str3);
+
+
+// var akd = 'Aditya' ;
+//  var arr = [] ;
+ 
+
+//  for ( var i=0; i<=akd.length-1; i++){
+//     if ( akd[i] === ('t')){
+//         arr.push('j')
+//     }
+//     else{
+//         arr.push(akd[i])
+//     }
+//  }
+//  console.log(akd);
+ 
+
+// nested loop
+
+// for ( var j=1; j<=5; j++){
+//     for ( var i=0; i<=5; i++){
+//         console.log(i);
+        
+//     }
+// }
+// console.log(j);
+for( var i=1; 1<=5; i++){
+    var bag = ' ' ;
+    for ( var j=1; j<=; j++){
+        bag += '*'
+        console.log(j);
+        
+    }
+}
+console.log(i);
